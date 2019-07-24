9787a3e15b7ca3bc5b81cd396f7860feba66bfd2
Mitchell Dill <malexanderdill@gmail.com>
2019-07-24 17:17:26 -0500

    Configure ESLint for AirBNB style guide

--- tests/table2.test.js
+++ tests/table2.test.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import Table2 from '../client/table2.js';
-import renderer from 'react-test-renderer';
+import React from "react";
+import Table2 from "../client/table2.js";
+import renderer from "react-test-renderer";
 
-it('continues to render Table2 component correctly', () => {
-    const tree = renderer
-      .create(<Table2 />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+it("continues to render Table2 component correctly", () => {
+  const tree = renderer.create(<Table2 />).toJSON();
+  expect(tree).toMatchSnapshot();
 });

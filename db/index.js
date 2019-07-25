a6e7ccaef8af0bc07ae8102b5c212c983abbcf7a
Mitchell Dill <malexanderdill@gmail.com>
2019-07-25 16:11:15 -0500

    Connect to logal PG DB, implement first tests

--- db/index.js
+++ db/index.js
@@ -1,37 +1,74 @@
-const mysql = require("mysql");
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const con = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.NAME,
-  password: process.env.PASS,
-  database: process.env.NAME
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT
 });
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log(process.env.HOST, "HOST");
-//   console.log("Connected database!");
-// });
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
-// going to get names for each item.
-// going to move all names to react array in order to filter for autofilling functionality
-const findAllNames = cb => {
-  // placeholder //put in .escape(user) format instead of ${}
-  con.query(`Select items.name from items`, (err, results) => {
-    if (err) cb(err);
-    cb(null, results);
-  });
+const getOneName = async () => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await pool.query(`SELECT * FROM tests;`);
+    console.log(`tests: ${rows[0].name}`);
+    return rows[0].name;
+  } finally {
+    client.release();
+  }
 };
 
-const getSpecificItem = (item, cb) => {
-  con.query(
-    `select * from items where items.name='${item.username}'`,
-    (err, results) => {
-      if (err) cb(err);
-      cb(null, results);
-    }
-  );
+const getAllNames = async () => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await pool.query(`SELECT * FROM tests limit 1;`);
+    console.log(`tests: ${rows}`);
+    return rows;
+  } finally {
+    client.release();
+  }
 };
 
-module.exports = { findAllNames, getSpecificItem };
+getAllNames().catch(e => console.log(e.stack));
+
+//   // async/await - check out a client
+//   async () => {
+//     const client = await pool.connect()
+//     try {
+//       const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
+//       console.log(res.rows[0])
+//     } finally {
+//       client.release()
+//     }
+//   }
+// )()
+// .catch(e => console.log(e.stack))
+
+// // going to get names for each item.
+// // going to move all names to react array in order to filter for autofilling functionality
+// const findAllNames = cb => {
+//   // placeholder //put in .escape(user) format instead of ${}
+//   con.query(`Select items.name from items`, (err, results) => {
+//     if (err) cb(err);
+//     cb(null, results);
+//   });
+// };
+
+// const getSpecificItem = (item, cb) => {
+//   con.query(
+//     `select * from items where items.name='${item.username}'`,
+//     (err, results) => {
+//       if (err) cb(err);
+//       cb(null, results);
+//     }
+//   );
+// };
+
+module.exports = { getAllNames, getOneName };
